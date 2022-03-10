97958f172e82d2dd02e89521acf32cc7787f5c65
clara.chalumeau <clara.chalumeau@epita.fr>
2022-03-10 15:05:44 +0100

    composition de logger

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -2,6 +2,6 @@ package fr.lernejo.logger;
 
 public class LoggerFactory {
     public static Logger getLogger(String name){
-        return new ContextualLogger(name, new FileLogger("log.txt"));
+        return new CompositeLogger(new FileLogger("log.txt"), new ContextualLogger(name, new ConsoleLogger()));
     }
 }
